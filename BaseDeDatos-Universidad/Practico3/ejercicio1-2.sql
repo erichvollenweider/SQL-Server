db913924259b915deed0628a65fe99738b9f1c68
Erich Vollenweider <101806079+erichvollenweider@users.noreply.github.com>
2024-04-29 09:56:59 -0300

    Add files via upload

--- BaseDeDatos-Universidad/Practico3/ejercicio1-2.sql
+++ BaseDeDatos-Universidad/Practico3/ejercicio1-2.sql
@@ -41,8 +41,8 @@ create table vacunas(
 create table vacunados(
 	dni int not null,
 	cod_vacuna int not null,
-	constraint FK_vacunados_personas foreign key (dni) references personas(dni),
-	constraint FK_vacunados_personas2 foreign key (cod_vacuna) references vacunas(cod_vacuna)
+	constraint FK_vacunados_personas foreign key (dni) references personas(dni), 
+	constraint FK_vacunados_personas2 foreign key (cod_vacuna) references vacunas(cod_vacuna) on delete cascade
 );
 
 -- Ejercicio 2
@@ -54,10 +54,27 @@ insert into Articulos values(4, 'Talco', 3500.25, 12, 15, 30, 'Diciembre', '2024
 select * from Articulos;
 
 insert into municipios values('Rio Cuarto', 'Cordoba');
-insert into personas values(33348556, 'Liones', 'Messi', 36,'Rio Cuarto');
+insert into personas values(33348556, 'Lionel', 'Messi', 36,'Rio Cuarto');
 insert into contagios values(33348556, 3);
 insert into vacunas values(1, 'COVID19');
 insert into vacunados values(33348556, 1);
+
+insert into municipios values('Laborde', 'Cordoba');
+insert into personas values(32260650, 'Cristiano', 'Ronaldo', 37,'Laborde');
+insert into contagios values(32260650, 2);
+insert into vacunas values(1, 'COVID19');
+insert into vacunados values(32260650, 1);
+
+insert into municipios values('Pascanas', 'Cordoba');
+insert into personas values(32444211, 'Sergio', 'Aguero', 37,'Pascanas');
+insert into contagios values(32444211, 2);
+insert into vacunas values(2, 'FIEBRE AMARILLA');
+insert into vacunados values(32444211, 2);
+
+insert into personas values(22565777, 'Mario', 'Bross', 41,'Laborde');
+insert into contagios values(22565777, 1);
+insert into vacunas values(3, 'DENGUE');
+insert into vacunados values(22565777, 3);
 
 select * from municipios;
 select * from personas;
@@ -66,4 +83,20 @@ select * from vacunas;
 select * from vacunados;
 
 
+-- Ejercicio 4
 
+-- alter table vacunados
+-- drop constraint FK_vacunados_personas2;
+
+-- alter table vacunados
+-- drop constraint FK_vacunados_personas;
+
+-- alter table vacunados
+-- add constraint FK_vacunados_personas foreign key (dni) references personas(dni);
+
+-- alter table vacunados
+-- add constraint FK_vacunados_personas2 foreign key (cod_vacuna) references vacunas(cod_vacuna) on delete cascade;
+
+delete from vacunas where cod_vacuna = 1;
+
+-- Ejercicio 5
