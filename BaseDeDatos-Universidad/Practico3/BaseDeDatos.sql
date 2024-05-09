8e135c12f53be303be8db11e5384dc7efff31036
Erich Vollenweider <101806079+erichvollenweider@users.noreply.github.com>
2024-05-09 14:04:04 -0300

    Add files via upload

--- BaseDeDatos-Universidad/Practico3/BaseDeDatos.sql
+++ BaseDeDatos-Universidad/Practico3/BaseDeDatos.sql
@@ -1,9 +1,24 @@
-CREATE TABLE Director(
+CREATE TABLE Personal(
 	nombre VARCHAR(50) NOT NULL,
     nacionalidad VARCHAR(20) NOT NULL,
     cantidad_peliculas INT NOT NULL,
-    CONSTRAINT PK_nombre_d PRIMARY KEY (nombre),
-    CONSTRAINT CK_cantidad_pelicula_d CHECK (cantidad_peliculas >= 0)
+    CONSTRAINT PK_nombre_p PRIMARY KEY (nombre),
+    CONSTRAINT CK_cantidad_pelicula_p CHECK (cantidad_peliculas >= 0)
+);
+
+CREATE TABLE Director(
+	nombre VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_nombre_director FOREIGN KEY (nombre) REFERENCES Personal(nombre)
+);
+
+CREATE TABLE Protagonista(
+	nombre VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_nombre_p FOREIGN KEY (nombre) REFERENCES Personal(nombre)
+);
+
+CREATE TABLE Reparto(
+	nombre VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_nombre_r FOREIGN KEY (nombre) REFERENCES Personal(nombre)
 );
 
 CREATE TABLE Pelicula(
@@ -29,22 +44,6 @@ CREATE TABLE Pelicula(
     CONSTRAINT CK_calificación CHECK (calificacion IN ('+ 13 años', '+ 15 años','+ 18 años'))
 );
 
-CREATE TABLE Protagonista(
-	nombre VARCHAR(50) NOT NULL,
-    nacionalidad VARCHAR(20) NOT NULL,
-    cantidad_peliculas INT NOT NULL,
-    CONSTRAINT PK_nombre_p PRIMARY KEY (nombre),
-    CONSTRAINT CK_cantidad_pelicula_p CHECK (cantidad_peliculas >= 0)
-);
-
-CREATE TABLE Reparto(
-	nombre VARCHAR(50) NOT NULL,
-    nacionalidad VARCHAR(20) NOT NULL,
-    cantidad_peliculas INT NOT NULL,
-    CONSTRAINT PK_nombre_r PRIMARY KEY (nombre),
-    CONSTRAINT CK_cantidad_pelicula_r CHECK (cantidad_peliculas >= 0)
-);
-
 CREATE TABLE Actuo(
 	identificador_pelicula INT NOT NULL,
     nombre_protagonista VARCHAR(50) NOT NULL,
@@ -58,58 +57,6 @@ CREATE TABLE Participo(
     CONSTRAINT FK_identificador_p_r FOREIGN KEY (identificador_pelicula) REFERENCES Pelicula(identificador),
     CONSTRAINT FK_nombre_reparto FOREIGN KEY (nombre_reparto) REFERENCES Reparto(nombre)
 );
-    
--- -----------------------INICIO TRIGGER ----------------------------
-DELIMITER $$
-
-CREATE TRIGGER insertar_actuo_despues_de_insertar_pelicula
-AFTER INSERT ON Pelicula
-FOR EACH ROW
-BEGIN
-    INSERT INTO Actuo (identificador_pelicula, nombre_protagonista) VALUES (NEW.identificador, NEW.nombre);
-END $$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER insertar_actuo_despues_de_insertar_protagonista
-AFTER INSERT ON Protagonista
-FOR EACH ROW
-BEGIN
-    INSERT INTO Actuo (identificador_pelicula, nombre_protagonista)
-    SELECT identificador, NEW.nombre FROM Pelicula;
-END $$
-
-DELIMITER ;
-
-
--- -----------------------FIN TRIGGER ----------------------------
-
--- -----------------------INICIO TRIGGER ----------------------------
-DELIMITER $$
-
-CREATE TRIGGER insertar_participo_despues_de_insertar_pelicula
-AFTER INSERT ON Pelicula
-FOR EACH ROW
-BEGIN
-    INSERT INTO Participo (identificador_pelicula, nombre_reparto) VALUES (NEW.identificador, NEW.nombre);
-END $$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER insertar_participo_despues_de_insertar_protagonista
-AFTER INSERT ON Reparto
-FOR EACH ROW
-BEGIN
-    INSERT INTO Participo (identificador_pelicula, nombre_reparto)
-    SELECT identificador, NEW.nombre FROM Pelicula;
-END $$
-
-DELIMITER ;
--- -----------------------FIN TRIGGER ----------------------------
     
 CREATE TABLE Cine(
 	nombre VARCHAR(50) NOT NULL,
@@ -144,9 +91,23 @@ CREATE TABLE Exhibe(
     CONSTRAINT FK_codigo_funcion_e FOREIGN KEY (codigo_funcion) REFERENCES Funcion(codigo)
 );
 
-INSERT INTO Director (nombre, nacionalidad, cantidad_peliculas) VALUES
-('Justin Lin', 'Taieanés', 6),
-('James Wan', 'Malayo', 1);
+-- INSERT INTO Personal (nombre, nacionalidad, cantidad_peliculas) VALUES
+-- ('Justin Lin', 'Taieanés', 6),
+-- ('James Wan', 'Malayo', 1);
+-- ('Vin Diesel', 'Estadounidense', 5),
+-- ('Paul Walker', 'Estadounidense', 6),
+-- ('Tyrese Gibson', 'Estadounidense', 5),
+-- ('Jordana Brewster', 'Panameño', 5);
+
+-- INSERT INTO Director (nombre) VALUES
+-- ('Justin Lin'),
+-- ('James Wan');
+
+-- INSERT INTO Protagonista (nombre) VALUES
+-- ('Vin Diesel'),
+-- ('Paul Walker'),
+-- ('Tyrese Gibson'),
+-- ('Jordana Brewster');
 
 INSERT INTO Pelicula (identificador, titulo_distribucion, titulo_original, titulo_español, genero, idioma_original, año_produccion, resumen, fecha_estreno, duracion, url, pais_origen, calificacion, nombre_director) VALUES
 (1, 'The Fast and The Furious', 'THE FAST AND THE FURIOUS', 'Rápido y Furioso', 'Acción', 'Inglés', 2001, 'Un policía encubierto se infiltra en una subcultura del inframundo de corredores callejeros de Los Ángeles que buscan reventar una red de secuestros, y pronto comienza a cuestionar sus lealtades.', '2001-06-22', '01:47:00', 'http://www.rapidosyfuriosos.com.ar/franchise.php?id=4', 'Estados Unidos', '+ 13 años', 'Justin Lin'),
@@ -157,18 +118,48 @@ INSERT INTO Pelicula (identificador, titulo_distribucion, titulo_original, titul
 (6, 'Fast & Furious 6', 'FAST & FURIOUS 6', 'Rápido y Furioso 6', 'Acción', 'Inglés', 2013, 'Desde que Dom y Brian destruyeron el imperio de un mafioso y se hicieron con cien millones de dólares, se encuentran en paradero desconocido; no pueden regresar a casa porque la ley los persigue. Entretanto, Hobbs ha seguido la pista por una docena de países a una banda de letales conductores mercenarios, cuyo cerebro cuenta con la inestimable ayuda de la sexy Letty, un viejo amor de Dom que éste daba por muerta. La única forma de detenerlos es enfrentarse a ellos en las calles, así que Hobbs le pide a Dom que reúna a su equipo en Londres. ¿Qué obtendrán a cambio? Un indulto para que todos puedan volver a casa con sus familias.', '2013-05-24', '02:10:00', 'https://www.imdb.com/title/tt1905041/', 'Estados Unidos', '+ 15 años', 'Justin Lin'),
 (7, 'Furious 7', 'FURIOUS 7', 'Rápidos y Furiosos 7', 'Acción', 'Inglés', 2015, 'Luego de haber derrotado al terrorista Owen Shaw, Dominic Toretto y sus amigos creían haber dejado la vida ruda atrás. Sin embargo, Deckard Shaw, el hermano de Owen, aparece de pronto para cobrar venganza. Su intención es eliminar al clan que exterminó a su hermano, uno por uno. ', '2015-04-03', '02:20:00', 'https://www.imdb.com/title/tt2820852/', 'Estados Unidos', '+ 15 años', 'James Wan');
 
-INSERT INTO Protagonista (nombre, nacionalidad, cantidad_peliculas) VALUES
-('Vin Diesel', 'Estadounidense', 5),
-('Paul Walker', 'Estadounidense', 6),
-('Tyrese Gibson', 'Estadounidense', 5),
-('Jordana Brewster', 'Panameño', 5);
-
--- INSERT INTO Reparto (nombre, nacionalidad, cantidad_peliculas) VALUES
--- ('Vin Diesel', 'Estadounidense', 5),
--- ('Paul Walker', 'Estadounidense', 6),
--- ('Tyrese Gibson', 'Estadounidense' 5),
--- ('Jordana Brewster', 'Panameño', 5);
-
 INSERT INTO Cine (nombre, direccion, telefono) VALUES
 ('Cine del Paseo', 'Sobremonte 80, Río Cuarto, Córdoba', '03584621235'),
 ('Cine Hoyts Patio Olmos', 'Vélez Sarsfield 361, Córdoba', '03514238408');
+
+-- -----------------------INICIO TRIGGER ----------------------------
+DELIMITER $$
+CREATE TRIGGER insertar_actuo_despues_de_insertar_pelicula
+AFTER INSERT ON Pelicula
+FOR EACH ROW
+BEGIN
+    INSERT INTO Actuo (identificador_pelicula, nombre_protagonista) VALUES (NEW.identificador, NEW.nombre);
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER insertar_actuo_despues_de_insertar_protagonista
+AFTER INSERT ON Protagonista
+FOR EACH ROW
+BEGIN
+    INSERT INTO Actuo (identificador_pelicula, nombre_protagonista)
+    SELECT identificador, NEW.nombre FROM Pelicula;
+END $$
+DELIMITER ;
+-- -----------------------FIN TRIGGER ----------------------------
+
+-- -----------------------INICIO TRIGGER ----------------------------
+DELIMITER $$
+CREATE TRIGGER insertar_participo_despues_de_insertar_pelicula
+AFTER INSERT ON Pelicula
+FOR EACH ROW
+BEGIN
+    INSERT INTO Participo (identificador_pelicula, nombre_reparto) VALUES (NEW.identificador, NEW.nombre);
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER insertar_participo_despues_de_insertar_protagonista
+AFTER INSERT ON Reparto
+FOR EACH ROW
+BEGIN
+    INSERT INTO Participo (identificador_pelicula, nombre_reparto)
+    SELECT identificador, NEW.nombre FROM Pelicula;
+END $$
+DELIMITER ;
+-- -----------------------FIN TRIGGER ----------------------------
