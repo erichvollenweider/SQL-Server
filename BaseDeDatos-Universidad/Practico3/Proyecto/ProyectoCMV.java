3edeea4719d71c4e219ee129e9bf3dd8b4769b17
Erich Vollenweider <101806079+erichvollenweider@users.noreply.github.com>
2024-06-06 20:52:52 -0300

    Add files via upload

---
+++ BaseDeDatos-Universidad/Practico3/Proyecto/ProyectoCMV.java
@@ -0,0 +1,281 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ProyectoCMV {
+  public static void main(String[] args) {
+
+	  Connection connection = null;
+
+    try {
+      String driver = "com.mysql.cj.jdbc.Driver";
+      String url = "jdbc:mysql://localhost:3306/ProyectoCMV";
+      String username = "root";
+      String password = "vllnwdr629";
+
+      // Cargamos el driver, conección con la base de datos y para trabajar con transacciones.
+      Class.forName(driver);
+      connection = DriverManager.getConnection(url, username, password);
+      connection.setAutoCommit(false); 
+
+      // Comenzamos el loop para utilizar de menu
+      boolean menu = false;
+      Scanner scanner = new Scanner(System.in);
+      boolean insertado = false;
+      String query;
+      PreparedStatement statement;
+      int res;
+
+      while(!menu) {
+        System.out.println();
+        System.out.println("Menú de opciones");
+        System.out.println("1. Insertar un cine");
+        System.out.println("2. Insertar una sala en un cine");
+        System.out.println("3. Listar los cines con la información de sus salas");
+        System.out.println("4. Devolver actores que solo figuran en una pelicula");
+        System.out.println("5. Listar las personas que han sido actores y directores");
+        System.out.println("6. Listas los cines con la cantidad total de butacas");
+        System.out.println("7. Consultas propias");
+        System.out.println("8. Salir");
+        System.out.print("Seleccione una de las opciones: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+
+        switch (choice) {
+          case 1:
+            query = "INSERT INTO Cine (NOMBRE,DIRECCION,TELEFONO) VALUES (?,?,?)";
+            statement = connection.prepareStatement(query);
+            insertado = false;
+            String nombre = "", dir = "", tel = "";
+            res = 0;
+      
+            while(!insertado) {
+              System.out.print("Inserte el nombre del nuevo cine: ");
+              nombre = scanner.nextLine();
+              System.out.print("Inserte la direcciòn del nuevo cine: ");
+              dir = scanner.nextLine();
+              System.out.print("Inserte el telefono del nuevo cine: ");
+              tel = scanner.nextLine();
+      
+              System.out.println("Nombre: "+nombre+" - Direcciòn: "+dir+" - Telefono: "+tel);
+              System.out.println("¿Los datos son correctos? SI(1)");
+              res = scanner.nextInt();
+              scanner.nextLine();
+              if (res == 1) {
+                insertado = true;
+              }
+            }
+      
+            statement.setString(1, nombre);
+            statement.setString(2, dir);
+            statement.setString(3, tel);
+            statement.executeUpdate();
+
+            //  Comiteo los cambios a la base de datos
+            connection.commit();
+
+            System.out.println("Cine agregado correctamente.");
+
+            break;
+          case 2:
+            insertado = false;
+            int numero = 0, numeroF = 0, cantidad_butacas = 0;
+            String nombre_cine = "";
+            res = 0;
+
+            while(!insertado) {
+              System.out.print("Inserte la cantidad de butacas de la sala: ");
+              cantidad_butacas = scanner.nextInt();
+              scanner.nextLine();
+              System.out.print("Inserte el nombre del cine al que pertenece la sala: ");
+              nombre_cine = scanner.nextLine();
+            
+              // Obtener el número de sala a crear
+              query = "SELECT COUNT(numero) AS max_numero FROM Sala";
+              statement = connection.prepareStatement(query);
+              ResultSet result = statement.executeQuery();
+              if (result.next()) {
+                numero = result.getInt("max_numero")+1;
+              } else {
+                numero = 1;
+              }
+
+              // Obtener el número a mostrar, el no real
+              query = "SELECT COUNT(numero) AS max_numero FROM Sala WHERE nombre_cine = ?";
+              statement = connection.prepareStatement(query);
+              statement.setString(1, nombre_cine);
+              result = statement.executeQuery();
+              if (result.next()) {
+                numeroF = result.getInt("max_numero")+1;
+              } else {
+                numeroF = 1;
+              }
+
+              System.out.println("Número de sala: "+numeroF+" - Cantidad de butacas: "+cantidad_butacas
+              +" - Nombre del cine: "+nombre_cine);
+              System.out.println("¿Los datos son correctos? SI(1)");
+              res = scanner.nextInt();
+              scanner.nextLine();
+              if (res == 1) {
+                insertado = true;
+              }
+            }
+
+            query = "INSERT INTO Sala (NUMERO, CANTIDAD_BUTACAS, NOMBRE_CINE) VALUES (?, ?, ?)";
+            statement = connection.prepareStatement(query);
+
+            statement.setInt(1, numero);
+            statement.setInt(2, cantidad_butacas);
+            statement.setString(3, nombre_cine);
+            statement.executeUpdate();
+            connection.commit();
+
+            System.out.println("Sala agregada correctamente.");
+
+            break;
+          case 3:
+            query = "SELECT c.nombre, s.numero, s.cantidad_butacas FROM Cine c "+
+            "LEFT JOIN Sala s ON c.nombre = s.nombre_cine ORDER BY c.nombre, "+
+            "s.numero";
+            statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+
+            System.out.println("Cines con la información de sus salas");
+            Map<String, Integer> cineSalaMap = new HashMap<>();
+            while(result.next()) {
+              String nombreCine = result.getString("nombre");
+              int cantidadButacas = result.getInt("cantidad_butacas");
+              
+              if (cineSalaMap.containsKey(nombreCine)) {
+                cineSalaMap.put(nombreCine, cineSalaMap.get(nombreCine)+1);
+              } else {
+                cineSalaMap.put(nombreCine, 1);
+              }
+
+              int numeroSalaMostrar = cineSalaMap.get(nombreCine);
+              System.out.println("Cine: "+nombreCine+" - Número sala: "+numeroSalaMostrar+" - Butacas en sala: "+cantidadButacas);
+            }
+
+            break;
+          case 4:
+            query = "SELECT nombre, COUNT(*) " +
+            "FROM ( " +
+            "SELECT nombre_p AS nombre FROM Actuo " +
+            "UNION ALL " +
+            "SELECT nombre_r AS nombre FROM Participo " +
+            ") AS nombres " +
+            "GROUP BY nombre " +
+            "HAVING COUNT(*) = 1";
+            statement = connection.prepareStatement(query);
+            result = statement.executeQuery();
+
+            System.out.println("Actores que solo estuvieron en una pelicula");
+            while (result.next()) {
+              String nombreActor = result.getString("nombre");
+              System.out.println("Actor: " + nombreActor);
+            }
+
+            break;
+          case 5:
+            query = "SELECT DISTINCT Personal.nombre " +
+            "FROM Personal " +
+            "JOIN Director ON Personal.nombre = Director.nombre_director " +
+            "JOIN Protagonista ON Personal.nombre = Protagonista.nombre_protagonista";
+            statement = connection.prepareStatement(query);
+            result = statement.executeQuery();
+
+            System.out.println("Personas que actuaron y dirigieron");
+            while(result.next()) {
+              nombre = result.getString("nombre");
+              System.out.println("Nombre: "+nombre);
+            }
+
+            break;
+          case 6:
+            query = "SELECT Cine.nombre, SUM(Sala.cantidad_butacas) AS total_butacas " +
+            "FROM Cine " +
+            "JOIN Sala ON Cine.nombre = Sala.nombre_cine " +
+            "GROUP BY Cine.nombre";
+            statement = connection.prepareStatement(query);
+            result = statement.executeQuery();
+
+            System.out.println("Cines con butacas totales");
+            while(result.next()) {
+              String nombreCine = result.getString("nombre");
+              int totalButacas = result.getInt("total_butacas");
+              System.out.println("Cine: "+nombreCine+" - Butacas totales: "+totalButacas);
+            }
+            break;
+          case 7:
+            // Peliculas que duran mas de 1 hora y 45 minutos
+            query = "SELECT titulo_español, duracion FROM Pelicula WHERE duracion > '01:45:00'";
+            statement = connection.prepareStatement(query);
+            result = statement.executeQuery();
+
+            System.out.println("Peliculas que duran más de 01:45:00");
+            while(result.next()) {
+              String titulo = result.getString("titulo_español");
+              Time duracion = result.getTime("duracion");
+              System.out.println("Titulo: "+titulo+" - Duración: "+duracion);
+            }
+            System.out.println();
+
+            // Peliculas entre 2010 y 2015
+            query = "SELECT titulo_español, año_produccion FROM Pelicula WHERE año_produccion BETWEEN 2010 AND 2015";
+            statement = connection.prepareStatement(query);
+            result = statement.executeQuery();
+
+            System.out.println("Peliculas entre 2010 y 2015");
+            while(result.next()) {
+              String titulo = result.getString("titulo_español");
+              int añoP = result.getInt("año_produccion");
+              System.out.println("Titulo: "+titulo+" - Año: "+añoP);
+            }
+            System.out.println();
+
+            // Devuelve los títulos de distribución de las películas que tienen al menos un actor protagonista que ha actuado en más de 3 películas
+            query = "SELECT titulo_distribucion FROM Pelicula WHERE id_pelicula IN (SELECT DISTINCT Actuo.ident_pelicula"+
+              " FROM Actuo JOIN Protagonista ON Actuo.nombre_p = Protagonista.nombre_protagonista WHERE "+
+              "Protagonista.nombre_protagonista IN (SELECT nombre_protagonista FROM Actuo GROUP BY "+
+              "nombre_protagonista HAVING COUNT(ident_pelicula) > 3))";
+            statement = connection.prepareStatement(query);
+            result = statement.executeQuery();
+
+            System.out.println("Películas con actores protagonistas que han actuado en más de 3 películas:");
+            while(result.next()) {
+              String titulo = result.getString("titulo_distribucion");
+              System.out.println("Título: "+titulo);
+            }
+
+            break;
+          case 8:
+            menu = true;
+            System.out.println("¡Adios!");
+            break;
+          default:
+            System.out.println("Opción no válida. Intente de nuevo.");
+        }
+      }
+
+      connection.close();
+     
+    } catch(ClassNotFoundException cnfe) {
+        System.err.println("Error loading driver: " + cnfe);
+    } catch(SQLException sqle) {
+        try	{
+        // como se produjo una excepcion en el acceso a la base de datos se debe hacer el rollback	
+        // Thread.sleep (20*1000); 	
+         System.err.println("antes rollback: " + sqle);
+         connection.rollback();
+         System.err.println("Error Se produjo una Excepcion accediendo a la base de datoas: " + sqle);
+         sqle.printStackTrace();
+        } 
+        catch(Exception e) {
+          //System.err.println("Error Ejecutando el rollback de la transaccion: " + e.getMessage());
+          e.printStackTrace();
+        }
+    }
+  }
+}
---
+++ BaseDeDatos-Universidad/Practico3/Proyecto/ProyectoCMV.sql
@@ -0,0 +1,290 @@
+--                          Universidad Nacional de Río Cuarto
+--                 Facultad de Ciencias Exactas, Físico-Químicas y Naturales
+--                              Departamento de Computación
+--                      Asignatura: BASES DE DATOS (1959) Año 2024
+
+--                             Trabajo práctico integrador
+
+-- Integrantes: -Conti, Bruno.
+--              -Mezzano, Joaquín.
+--              -Vollenweider, Erich.
+
+-- DESCRIPCION DEL PROBLEMA:
+-- Se desea crear un sitio web con información referente a las películas en cartel
+-- en las salas de un complejo de cines cercano a un conocido centro de compras.
+-- De cada película, se almacena una ficha con su título de distribución, su título
+-- original, su género, el idioma original, el título en español, los países de origen
+-- de la producción, el año de la producción, la url del sitio web de la película, la
+-- duración (en horas y minutos), la calificación (Apta todo público,+13 años, +15
+-- años,+18 años), fecha de estreno en Argentina, un resumen y un identificador de la película.
+-- De cada película interesa conocer la lista de directores, actores protagonistas y los de reparto.
+-- De los directores y actores, se conoce su nombre (que lo identifica) y su nacionalidad.
+-- Además se desea conocer la cantidad de películas en las que
+-- dirigieron o actuaron. Tenga en cuenta que hay personas que cumplen los dos roles.
+-- Los cines pueden tener más de una sala y cada semana cada uno de los cines
+-- envía la cartelera para dicha semana, indicando el detalle de las funciones.
+-- Para cada función se conoce, su código (único para todo el complejo), la fecha,
+-- la hora de comienzo, y obviamente la sala y la película que exhibe. De cada
+-- sala se sabe su número que la identifica dentro del complejo (no se repite por
+-- cine) y la cantidad de butacas que posee. De cada cine se conoce el nombre
+-- que lo identifica, su dirección y teléfono para consultas.
+
+-- Consideración a tener en cuenta en la implementación de la base de datos en un motor de base de datos:
+-- · Utilice códigos cuando lo crea conveniente.
+-- · Los diferentes códigos deben ser generados automáticamente.
+-- · Crear un dominio para la calificación de las películas.
+-- · Todos los códigos o números identificatorios deben ser enteros positivos.
+-- · Los títulos originales de las películas deben ser ingresados, si o si en mayúsculas.
+-- · Se debe llevar una tabla de auditoria donde quede registrado todos los cambios que se han hecho en la fecha de estreno de una película y cuando fueron hechos.
+-- · Agregue las restricciones de dominio que crea convenientes
+
+-- Tareas a Realizar Utilizando el lenguaje SQL:
+-- 3. Definir el diccionario de datos creando:
+--  . La base de datos.
+--  . Las tablas componentes con todas las restricciones que el problema requiere
+--    (claves primarias, claves foráneas indicando que hacer al borrar o actualizar, etc.). Implementar las restricciones de tipos.
+-- 4. Generar un script SQL para la carga de información en la base de datos
+--    (archivo de texto con el código SQL para la inserción de datos).
+
+CREATE DATABASE ProyectoCMV;
+USE ProyectoCMV;
+
+CREATE TABLE Personal(
+	nombre VARCHAR(50) NOT NULL,
+    nacionalidad VARCHAR(20) NOT NULL,
+    cantidad_peliculas INT NOT NULL,
+    CONSTRAINT PK_nombre_p PRIMARY KEY (nombre),
+    CONSTRAINT CK_cantidad_pelicula_p CHECK (cantidad_peliculas >= 0)
+);
+
+CREATE TABLE Director(
+	nombre_director VARCHAR(50),
+    CONSTRAINT PK_nombre_director FOREIGN KEY (nombre_director) REFERENCES Personal(nombre) ON DELETE SET NULL
+);
+
+CREATE TABLE Protagonista(
+	nombre_protagonista VARCHAR(50),
+    CONSTRAINT PK_nombre_p FOREIGN KEY (nombre_protagonista) REFERENCES Personal(nombre) ON DELETE SET NULL
+);
+
+CREATE TABLE Reparto(
+	nombre_reparto VARCHAR(50),
+    CONSTRAINT PK_nombre_r FOREIGN KEY (nombre_reparto) REFERENCES Personal(nombre) ON DELETE SET NULL
+);
+
+CREATE TABLE Pelicula(
+	id_pelicula INT NOT NULL,
+    titulo_distribucion VARCHAR(50) NOT NULL,
+    titulo_original VARCHAR(50) NOT NULL,
+    titulo_español VARCHAR(50) NOT NULL,
+    genero VARCHAR(20) NOT NULL,
+    idioma_original VARCHAR(20) NOT NULL,
+    año_produccion INT NOT NULL,
+    resumen VARCHAR(1000) NOT NULL,
+    fecha_estreno DATE NOT NULL,
+    duracion TIME NOT NULL,
+    url VARCHAR(100) NOT NULL,
+    calificacion VARCHAR(20) NOT NULL DEFAULT 'Apta todo público',
+    nombre_d VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_identificador PRIMARY KEY (id_pelicula),
+    CONSTRAINT UK_fecha_estreno UNIQUE KEY (fecha_estreno),
+    CONSTRAINT CK_identificador CHECK (id_pelicula >= 0),
+    CONSTRAINT CK_año_produccion CHECK (año_produccion >= 0),
+    CONSTRAINT FK_nombre_d FOREIGN KEY (nombre_d) REFERENCES Director(nombre_director),
+    CONSTRAINT CK_calificación CHECK (calificacion IN ('+ 13 años', '+ 15 años','+ 18 años'))
+);
+
+DELIMITER $$
+
+CREATE TRIGGER after_insert_pelicula
+BEFORE INSERT ON Pelicula
+FOR EACH ROW
+BEGIN
+	SET NEW.titulo_original = UPPER(NEW.titulo_original);
+END $$
+
+DELIMITER ;
+
+CREATE TABLE MPelicula(
+	id_pelicula INT,
+    paises_origen VARCHAR(20),
+    CONSTRAINT FK_id_pel FOREIGN KEY (id_pelicula) REFERENCES Pelicula(id_pelicula) ON DELETE CASCADE
+);
+
+CREATE TABLE Actuo(
+	ident_pelicula INT,
+    nombre_p VARCHAR(50),
+    CONSTRAINT FK_identificador_p_p FOREIGN KEY (ident_pelicula) REFERENCES Pelicula(id_pelicula) ON DELETE CASCADE,
+    CONSTRAINT FK_nombre_p FOREIGN KEY (nombre_p) REFERENCES Protagonista(nombre_protagonista)
+);
+
+CREATE TABLE Participo(
+	ident_pelicula INT NOT NULL,
+    nombre_r VARCHAR(50) NOT NULL,
+    CONSTRAINT FK_identificador_p_r FOREIGN KEY (ident_pelicula) REFERENCES Pelicula(id_pelicula) ON DELETE CASCADE,
+    CONSTRAINT FK_nombre_r FOREIGN KEY (nombre_r) REFERENCES Reparto(nombre_reparto)
+);
+    
+CREATE TABLE Cine(
+	nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+	CONSTRAINT PK_nombre_cine PRIMARY KEY (nombre)
+);
+
+CREATE TABLE Sala(
+	numero INT NOT NULL,
+    cantidad_butacas INT NOT NULL,
+    nombre_cine VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_numero PRIMARY KEY (numero),
+    CONSTRAINT CK_numero CHECK (numero >= 0),
+    CONSTRAINT FK_nombre_cine_s FOREIGN KEY (nombre_cine) REFERENCES Cine(nombre) ON DELETE CASCADE
+);
+
+CREATE TABLE Funcion(
+	codigo INT NOT NULL,
+    fecha DATE NOT NULL,
+    hora_comienzo TIME NOT NULL,
+    numero_sala INT,
+    id_peli INT,
+    CONSTRAINT PK_codigo PRIMARY KEY (codigo),
+    CONSTRAINT CK_codigo CHECK (codigo >= 0),
+    CONSTRAINT FK_numero FOREIGN KEY (numero_sala) REFERENCES Sala(numero),
+    CONSTRAINT FK_id_peli FOREIGN KEY (id_peli) REFERENCES Pelicula(id_pelicula)
+);
+
+CREATE TABLE Auditoria (
+	id_peli INT,
+    titulo_distribucion VARCHAR(50),
+    fecha_estreno_anterior DATE,
+    fecha_estreno_nueva DATE,
+    fecha_modificacion DATETIME,
+    CONSTRAINT FK_id_p FOREIGN KEY (id_peli) REFERENCES Pelicula(id_pelicula)
+);
+
+DELIMITER $$
+
+CREATE TRIGGER after_update_fecha
+AFTER UPDATE ON Pelicula
+FOR EACH ROW
+BEGIN
+	IF (NEW.fecha_estreno <> OLD.fecha_estreno) THEN
+		INSERT INTO Auditoria(id_peli, titulo_distribucion, fecha_estreno_anterior, fecha_estreno_nueva, fecha_modificacion)
+        VALUES (NEW.id_pelicula, NEW.titulo_distribucion, OLD.fecha_estreno, NEW.fecha_estreno, now());
+	END IF;
+END $$
+
+DELIMITER ;
+
+INSERT INTO Personal (nombre, nacionalidad, cantidad_peliculas) VALUES
+('Justin Lin', 'Taieanés', 6),
+('James Wan', 'Malayo', 1),
+('Vin Diesel', 'Estadounidense', 5),
+('Paul Walker', 'Estadounidense', 6),
+('Tyrese Gibson', 'Estadounidense', 5),
+('Jordana Brewster', 'Panameño', 5),
+('Chad Lindberg', 'Estadounidense', 1),
+('Johnny Strong', 'Estadounidense', 4),
+('Reggie Lee', 'Filipino', 1),
+('Jon Favreau', 'Estadounidense', 1),
+('Natalie Portman', 'Jerosolimitano', 1),
+('Angelina Jolie', 'Estadounidense', 1);
+
+INSERT INTO Director (nombre_director) VALUES
+('Justin Lin'),
+('James Wan'),
+('Jon Favreau'),
+('Natalie Portman'),
+('Angelina Jolie');
+
+INSERT INTO Protagonista (nombre_protagonista) VALUES
+('Vin Diesel'),
+('Paul Walker'),
+('Tyrese Gibson'),
+('Jordana Brewster'),
+('Natalie Portman');
+
+INSERT INTO Reparto (nombre_reparto) VALUES
+('Chad Lindberg'),
+('Johnny Strong'),
+('Reggie Lee'),
+('Jon Favreau');
+
+INSERT INTO Pelicula (id_pelicula, titulo_distribucion, titulo_original, titulo_español, genero, idioma_original, año_produccion, resumen, fecha_estreno, duracion, url, calificacion, nombre_d) VALUES
+(1, 'The Fast and The Furious', 'the fast AND THE FURIOUS', 'Rápido y Furioso', 'Acción', 'Inglés', 2001, 'Un policía encubierto se infiltra en una subcultura del inframundo de corredores callejeros de Los Ángeles que buscan reventar una red de secuestros, y pronto comienza a cuestionar sus lealtades.', '2001-06-22', '01:47:00', 'http://www.rapidosyfuriosos.com.ar/franchise.php?id=4', '+ 13 años', 'Justin Lin'),
+(2, '2 Fast 2 Furious', '2 fast 2 furious', 'Más Rápidos Más Furiosos', 'Acción', 'Inglés', 2003, 'Cuando el ex policía, Brian O`Conner, es capturado en Miami por su antiguo socio, Bilkins, lo reclutan para acabar con un narcotraficante llamado Carter Verone. O`Connor acepta ayudarlos en los términos de crear su propio equipo. Decide formar equipo con su amigo de la infancia, Roman Pearce.', '2003-06-06', '01:47:00', 'http://www.rapidosyfuriosos.com.ar/franchise.php?id=4', '+ 13 años', 'Justin Lin'),
+(3, 'The Fast and the Furious: Tokyo Drift', 'tHe fast aNd the furious: TOKYO DRIFT', 'Rápido y furioso: Reto Tokio', 'Acción', 'Inglés', 2006, 'Sean es un chico que no se adapta a ningún grupo, su única conexión con el mundo son las carreras ilegales, lo que lo ha convertido en el perseguido número uno por la policía. Cuando lo amenazan de cárcel, lo mandan a Japón con su padre. Estando en este país es atraído por el último reto automovilístico que desafía la gravedad: las carreras drift, una mezcla peligrosa de alta velocidad en pistas con curvas muy cerradas y en zigzag. Estas carreras lo llevan a involucrase con la mafia japonesa, el hampa de Tokio y a jugarse la vida.', '2006-06-16', '01:44:00', 'http://www.rapidosyfuriosos.com.ar/franchise.php?id=4', '+ 13 años', 'Justin Lin'),
+(4, 'Fast & Furious', 'fast & furious', 'Rápidos y Furiosos', 'Acción', 'Inglés', '2009', 'Un asesinato obliga a Don Toretto, un ex convicto huido, y al agente Brian O`Conner a volver a Los Ángeles. donde su pelea se reaviva. Pero al tener que enfrentarse a un enemigo común, se ven obligados a formar una alianza incierta si quieren conseguir desbaratar sus planes.', '2009-04-03', '01:47:00', 'http://www.rapidosyfuriosos.com.ar/franchise.php?id=4', '+ 13 años', 'Justin Lin'),
+(5, 'Fast Five', 'fast five', 'Rápidos y Furiosos 5in Control', 'Acción', 'Inglés', 2011, 'Luke Hobbs, un duro agente federal acostumbrado a dar caza a todos sus objetivos, debe confiar en su instinto para atraparles y que nadie más se le adelante. De esta forma, él y su equipo se embarcarán en un veloz viaje sin tregua para frenar a los protagonistas de la saga.', '2011-04-29', '02:11:00', 'https://www.imdb.com/title/tt1596343/', '+ 15 años', 'Justin Lin'),
+(6, 'Fast & Furious 6', 'fasT & FurIouS 6', 'Rápido y Furioso 6', 'Acción', 'Inglés', 2013, 'Desde que Dom y Brian destruyeron el imperio de un mafioso y se hicieron con cien millones de dólares, se encuentran en paradero desconocido; no pueden regresar a casa porque la ley los persigue. Entretanto, Hobbs ha seguido la pista por una docena de países a una banda de letales conductores mercenarios, cuyo cerebro cuenta con la inestimable ayuda de la sexy Letty, un viejo amor de Dom que éste daba por muerta. La única forma de detenerlos es enfrentarse a ellos en las calles, así que Hobbs le pide a Dom que reúna a su equipo en Londres. ¿Qué obtendrán a cambio? Un indulto para que todos puedan volver a casa con sus familias.', '2013-05-24', '02:10:00', 'https://www.imdb.com/title/tt1905041/', '+ 15 años', 'Justin Lin'),
+(7, 'Furious 7', 'furious 7', 'Rápidos y Furiosos 7', 'Acción', 'Inglés', 2015, 'Luego de haber derrotado al terrorista Owen Shaw, Dominic Toretto y sus amigos creían haber dejado la vida ruda atrás. Sin embargo, Deckard Shaw, el hermano de Owen, aparece de pronto para cobrar venganza. Su intención es eliminar al clan que exterminó a su hermano, uno por uno. ', '2015-04-03', '02:20:00', 'https://www.imdb.com/title/tt2820852/', '+ 15 años', 'James Wan'),
+(8, 'Iron Man', 'IRON MAN', 'El hombre de hierro', 'Acción', 'Inglés', 2008, 'Tony Stark es un inventor de armamento brillante que es secuestrado en el extranjero. Sus captores son unos terroristas que le obligan a construir una máquina destructiva pero Tony se construirá una armadura para poder enfrentarse a ellos y escapar.', '2008-04-30', '02:06:00', 'https://www.marvel.com/', '+ 15 años', 'Jon Favreau'),
+(9, 'A Tale of Love and Darkness', 'A Tale of Love and Darkness', 'Un historia de amor y oscuridad', 'Belico/Romance', 'Inglés', 2015, 'Basada en las memorias de Amos Oz, uno de los primeros escritores judíos que abogó por encontrar una solución al eterno conflicto entre Israel y Palestina. Desde su propio punto de vista, en aquel momento era tan sólo un niño, el autor aborda la creación del Estado de Israel. A los doce años, su madre se suicida y su vida queda marcada para siempre por este hecho. Tras esta tragedia, comienza a trabajar como escritor, participando activamente en la vida política del país.', '2015-09-14', '01:35:00', 'NULL', '+ 13 años', 'Natalie Portman'),
+(10, 'In The Land of Blood and Honey', 'In The Land of Blood and Honey', 'En tierra de sangre y miel', 'Belico/Romance', 'Inglés', 2011, 'Antes de la guerra de los Balcanes (años 90), Danijel, un policía serbio, y Ajla, una joven artista bosnia de religión musulmana, se conocieron y se enamoraron, pero la guerra y la terrible violencia que asoló el país los cambió para siempre. Al verse involucrados en el conflicto, su relación y la lealtad que se profesaban empezaron a desmoronarse.', '2011-12-23', '02:07:00', 'NULL', '+ 13 años', 'Angelina Jolie');
+
+INSERT INTO MPelicula (id_pelicula, paises_origen) VALUES
+(1, 'Estados Unidos'),
+(2, 'Estados Unidos'),
+(3, 'Estados Unidos'),
+(4, 'Estados Unidos'),
+(5, 'Estados Unidos'),
+(6, 'Estados Unidos'),
+(7, 'Estados Unidos'),
+(8, 'Estados Unidos'),
+(9, 'Estados Unidos'),
+(10, 'Estados Unidos');
+
+INSERT INTO Actuo (ident_pelicula, nombre_p) VALUES
+(1,'Vin Diesel'),
+(4,'Vin Diesel'),
+(5,'Vin Diesel'),
+(6,'Vin Diesel'),
+(7,'Vin Diesel'),
+(1, 'Paul Walker'),
+(2, 'Paul Walker'),
+(4, 'Paul Walker'),
+(5, 'Paul Walker'),
+(6, 'Paul Walker'),
+(7, 'Paul Walker'),
+(2, 'Tyrese Gibson'),
+(4, 'Tyrese Gibson'),
+(5, 'Tyrese Gibson'),
+(6, 'Tyrese Gibson'),
+(7, 'Tyrese Gibson'),
+(1, 'Jordana Brewster'),
+(4, 'Jordana Brewster'),
+(5, 'Jordana Brewster'),
+(6, 'Jordana Brewster'),
+(7, 'Jordana Brewster'),
+(9, 'Natalie Portman');
+
+INSERT INTO Participo (ident_pelicula, nombre_r) VALUES
+(1, 'Chad Lindberg'),
+(1, 'Johnny Strong'),
+(5, 'Johnny Strong'),
+(6, 'Johnny Strong'),
+(7, 'Johnny Strong'),
+(1, 'Reggie Lee'),
+(8, 'Jon Favreau');
+
+INSERT INTO Cine (nombre, direccion, telefono) VALUES
+('Cine del Paseo', 'Sobremonte 80, Río Cuarto, Córdoba', '03584621235'),
+('Cine Hoyts Patio Olmos', 'Vélez Sarsfield 361, Córdoba', '03514238408');
+
+INSERT INTO Sala (numero, cantidad_butacas, nombre_cine) VALUES
+(1, 50, 'Cine del Paseo'),
+(2, 75, 'Cine del Paseo'),
+(3, 60, 'Cine del Paseo'),
+(4, 45, 'Cine del Paseo'),
+(5, 100, 'Cine Hoyts Patio Olmos'),
+(6, 110, 'Cine Hoyts Patio Olmos'),
+(7, 80, 'Cine Hoyts Patio Olmos');
+
+INSERT INTO Funcion (codigo, fecha, hora_comienzo, numero_sala, id_peli) VALUES
+(1, '2024-05-12', '22:30:00', 1, 1),
+(2, '2024-05-12', '23:00:00', 2, 2),
+(3, '2024-05-13', '20:30:00', 1, 1),
+(4, '2024-05-12', '18:30:00', 3, 3),
+(5, '2024-05-14', '18:00:00', 3, 3),
+(6, '2024-05-10', '14:00:00', 5, 4),
+(7, '2024-05-15', '15:30:00', 6, 5);
